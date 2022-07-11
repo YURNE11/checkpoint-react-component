1aea231efa288e23cc2f91279a916344792f49c9
YURNE11 <103185020+YURNE11@users.noreply.github.com>
2022-07-11 17:26:20 +0200

    Add files via upload

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Address = () => {
+    return (
+        <div>
+            Tokyo Tower, 4 Chome-2-8 Shibakoen, Minato, Tokyo 105-0011
+        </div>
+    );
+}
+
+export default Address;
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import paire from '../../paire.png';
+const ProfilPhoto = () => {
+    return (
+        <div>
+           <img height="500" width="800" src={paire}/>
+        </div>
+    );
+}
+
+export default ProfilPhoto;
